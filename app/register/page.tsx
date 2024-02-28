d0c950defdd6b066f73158b7880ad81d1fca8d20
Yash <gupta25yash@gmail.com>
2024-02-28 12:43:56 +0530

    1st Commit and push

---
+++ app/register/page.tsx
@@ -0,0 +1,120 @@
+"use client"
+import React, { useContext } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import UserContext from '../../contexts/UserContexts';
+import Link from 'next/link'
+import { useState } from "react";
+export default function page(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+
+    const checkPassword=() => {
+        if(password != confirmPassword){
+            toast.error('password didnt matched ')
+        }
+        else if (!password){
+          toast.error('password is empty')
+        }
+        else if(!email){
+          toast.error('email is empty')
+        }
+        else if(!confirmPassword){
+          toast.error('confirm password is empty')
+        }
+        else{
+          toast.success('Registered')
+          module.exports(email, password)
+    }
+  };
+
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-md w-full space-y-8">
+            <div>
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Create your account
+              </h2>
+            </div>
+            <form className="mt-8 space-y-6" action="#" method="POST">
+              <input type="hidden" name="remember" value="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                    value={email}
+                    onChange={(e)=>{
+                        setEmail(e.target.value);
+                        console.log(e.target.value);
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e)=>{
+                        setPassword(e.target.value);
+                        console.log(e.target.value);
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="re-Type Password"
+                    value={confirmPassword}
+                    onChange={(e)=>{
+                        setConfirmPassword(e.target.value);
+                        console.log(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+    <ToastContainer/>
+              <div>
+                <button
+                  onClick= {checkPassword}
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+            <div className="flex">
+              <h1>Already have an account ?</h1>
+              <Link href="/login" className="text-blue-600">Login</Link>
+            </div>
+          </div>
+        </div>
+      );
+}
